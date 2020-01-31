2612ad0ad0c010a7d330f58da9af7f437e9583cc
Omkar Manjrekar <manjrekarom@gmail.com>
2020-01-31 14:35:13 +0530

    Rate of flow and total fluid calculation

--- flow_meter_interfacing/src/main.c
+++ flow_meter_interfacing/src/main.c
@@ -18,28 +18,47 @@
 #include "mgos.h"
 #define PIN 23
 
-volatile int flow_frequency = 0;
+volatile int pulse_frequency = 0;
+volatile int pulse_per_second = 0;
 
-float calc_litres(int flow_freq) {
+float litre_per_min;
+
+float calc_litres(int pulse_frequency) {
   // https://saiersensor.en.alibaba.com/product/60539487700-803861114/
   // SEN_HZ21WA_flow_sensor_liquid_level_sensor_magnetic_water_flow_sensor.html
   // 1L Water = 516 Pulse
-  return flow_freq / 516.0;
+  // OR https://www.e-gizmo.net/oc/kits%20documents/SEN-HZ21WA%20Flowmeter/SEN-HZ21WA%20Flowmeter.pdf
+  // flow_freq * 2.25 / 1000;
+  return pulse_frequency / 516.0;
 }
 
-static void timer_cb(void *arg) {
-  LOG(LL_INFO, ("Flow pulses: %d", flow_frequency));
-  LOG(LL_INFO, ("Quantity in Litres: %.2f", calc_litres(flow_frequency)));
+void calc_rate(void* arg) {
+  // Litres per min
+  mgos_gpio_disable_int(PIN);
+  litre_per_min = (pulse_per_second * 60.0) / 1000;
+  pulse_per_second = 0;
+  (void)arg;
+  mgos_gpio_enable_int(PIN);
 }
 
-void isr_cb(int pin, void *arg) {
-  flow_frequency++;
+static void isr_cb(int pin, void *arg) {
+  pulse_frequency++;
+  pulse_per_second++;
+}
+
+static void report(void *arg) {
+  mgos_gpio_disable_int(PIN);
+  LOG(LL_INFO, ("Pulse per second: %d & Total pulse count: %d", pulse_per_second, pulse_frequency));
+  LOG(LL_INFO, ("Quantity: %.2f Litres", calc_litres(pulse_frequency)));
+  LOG(LL_INFO, ("Rate: %.2f Litres/min", litre_per_min));
+  mgos_gpio_enable_int(PIN);
 }
 
 enum mgos_app_init_result mgos_app_init(void) {
-  mgos_set_timer(5000 /* ms */, MGOS_TIMER_REPEAT, timer_cb, NULL);
+  mgos_set_timer(3000 /* ms */, MGOS_TIMER_REPEAT, report, NULL);
+  mgos_set_timer(1000 /* ms */, MGOS_TIMER_REPEAT, calc_rate, NULL);
   mgos_gpio_setup_input(PIN, MGOS_GPIO_PULL_UP);
   mgos_gpio_set_int_handler_isr(PIN, MGOS_GPIO_INT_EDGE_NEG, isr_cb, NULL);
-  mgos_gpio_enable_int(PIN);
+  mgos_gpio_enable_int(PIN);  
   return MGOS_APP_INIT_SUCCESS;
 }
