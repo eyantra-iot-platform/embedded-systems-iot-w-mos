a8ada55d04288d996f1d36af2c8302fcf5bbda1e
Omkar Manjrekar <manjrekarom@gmail.com>
2020-02-03 19:57:35 +0530

    Added SH1106

--- oled_interfacing/src/main.c
+++ oled_interfacing/src/main.c
@@ -1,27 +1,27 @@
 #include "mgos.h"
-#include "ssd1306.h"
+#include "sh1106.h"
 
 enum mgos_app_init_result mgos_app_init(void) {
-  // bool oled_init = mgos_ssd1306_init();
+  // bool oled_init = mgos_sh1106_init();
   // if (!oled_init)
   //   return MGOS_APP_INIT_ERROR;
-  // struct mgos_ssd1306* oled = mgos_ssd1306_get_global();
-  // mgos_ssd1306_draw_string(oled, mgos_sys_config_get_x(), mgos_sys_config_get_y(), "Hello OLED!");
-  // // mgos_ssd1306_draw_circle(oled, 20, 20, 10, SSD1306_COLOR_WHITE);
-  // mgos_ssd1306_fill_rectangle(oled, 20, 20, 50, 30, SSD1306_COLOR_WHITE);
-  // mgos_ssd1306_refresh(oled, false);
+  // struct mgos_sh1106* oled = mgos_sh1106_get_global();
+  // mgos_sh1106_draw_string(oled, mgos_sys_config_get_x(), mgos_sys_config_get_y(), "Hello OLED!");
+  // // mgos_sh1106_draw_circle(oled, 20, 20, 10, SH1106_COLOR_WHITE);
+  // mgos_sh1106_fill_rectangle(oled, 20, 20, 50, 30, SH1106_COLOR_WHITE);
+  // mgos_sh1106_refresh(oled, false);
   // return MGOS_APP_INIT_SUCCESS;
-  bool oled_init = mgos_ssd1306_init();
+  bool oled_init = mgos_sh1106_init();
   if (!oled_init)
     return MGOS_APP_INIT_ERROR;
-  struct mgos_ssd1306* oled = mgos_ssd1306_get_global();
-  mgos_ssd1306_clear(oled);
-  mgos_ssd1306_draw_string(oled, mgos_sys_config_get_x(), mgos_sys_config_get_y(), "Lorem ipsum dolor sit");
-  mgos_ssd1306_draw_circle(oled, 20, 20, 10, SSD1306_COLOR_WHITE);
-  // mgos_ssd1306_fill_rectangle(oled, 20, 20, 50, 30, SSD1306_COLOR_WHITE);
-  mgos_ssd1306_draw_pixel(oled, 64, 32, SSD1306_COLOR_WHITE);
-  mgos_ssd1306_flip_display(oled, true, true);
-  // mgos_ssd1306_invert_display(oled, true);
-  mgos_ssd1306_refresh(oled, false);
+  struct mgos_sh1106* oled = mgos_sh1106_get_global();
+  mgos_sh1106_clear(oled);
+  mgos_sh1106_draw_string(oled, mgos_sys_config_get_x(), mgos_sys_config_get_y(), "Lorem ipsum dolor sit");
+  mgos_sh1106_draw_circle(oled, 20, 20, 10, SH1106_COLOR_WHITE);
+  // mgos_sh1106_fill_rectangle(oled, 20, 20, 50, 30, sh1106_COLOR_WHITE);
+  mgos_sh1106_draw_pixel(oled, 64, 32, SH1106_COLOR_WHITE);
+  mgos_sh1106_flip_display(oled, true, true);
+  // mgos_sh1106_invert_display(oled, true);
+  mgos_sh1106_refresh(oled, false);
   return MGOS_APP_INIT_SUCCESS;
 }
